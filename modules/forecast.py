3b75c653cee0fcd4d0fabb940589bc48f14e04e1
krithikag23 <113279833+krithikag23@users.noreply.github.com>
2025-11-16 12:27:02 +0530

    update

--- modules/forecast.py
+++ modules/forecast.py
@@ -1,6 +1,10 @@
 import pandas as pd
 
 def forecast_price(series):
+    # Return None if insufficient data
+    if len(series) == 0:
+        return None
+    
     last_price = series.iloc[-1]
     forecast_values = [last_price * (1 + 0.01*i) for i in range(1, 6)]
     future_index = pd.date_range(start=series.index[-1], periods=5, freq='D')
